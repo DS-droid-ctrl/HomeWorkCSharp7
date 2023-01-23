01294ecf5eb2b5068e27c7cfbb2ff1db532e4246
DenisM <mdr2010@yandex.ru>
2023-01-23 22:22:09 +0500

    add Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Clear();
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine("Введите искомое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+bool f = false;
+FillArraySearch(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void FillArraySearch(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matrix[i, j] == x) { f = true; };
+        }
+    }
+    if (f)
+    {
+        Console.WriteLine($"Число {x} в массиве есть");
+    }
+    else
+    {
+        Console.WriteLine($"Такого числа в массиве нет");
+    }
+}
